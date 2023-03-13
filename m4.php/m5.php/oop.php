8eac1ec16e23bb3fa8fcd8e57cbdb82a14aa5cfe
Toufiq Elahi1 <toufiqelahi@gmail.com>
2023-03-13 17:25:42 +0600

    Push existing project to GitLab

--- m4.php/m5.php/oop.php
+++ m4.php/m5.php/oop.php
@@ -1,80 +1,70 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>User Registration Form</title>
-</head>
-<body>
-	<h2>User Registration Form</h2>
-	<form action="process_form.php" method="POST" enctype="multipart/form-data">
-		<label for="name">Name:</label>
-		<input type="text" id="name" name="name" required><br><br>
-		<label for="email">Email:</label>
-		<input type="email" id="email" name="email" required><br><br>
-		<label for="password">Password:</label>
-		<input type="password" id="password" name="password" required><br><br>
-		<label for="profile_picture">Profile Picture:</label>
-		<input type="file" id="profile_picture" name="profile_picture" required><br><br>
-		<input type="submit" name="submit" value="Submit">
-	</form>
-</body>
-</html>
+<form>
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required><br><br>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required><br><br>
+
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password" required><br><br>
+
+  <label for="profile-pic">Profile Picture:</label>
+  <input type="file" id="profile-pic" name="profile-pic"><br><br>
+
+  <input type="submit" value="Submit">
+</form>
+
+
+
 
 
 <?php
 session_start();
-if(isset($_POST['submit'])){
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$profile_picture = $_FILES['profile_picture'];
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($profile_picture['name']);
-	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	$unique_file_name = uniqid() . '_' . date('Y-m-d-H-i-s') . '.' . $imageFileType;
-	$target_path = $target_dir . $unique_file_name;
 
 
-	if(empty($name) || empty($email) || empty($password) || empty($profile_picture['name'])){
-		echo "Please fill out all fields.";
-	} else {
-	
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-			echo "Please enter a valid email address.";
-		} else {
-			
-			if(file_exists($target_file)){
-				echo "Sorry, file already exists.";
-			} else {
-		
-				if($profile_picture['size'] > 5000000){
-					echo "Sorry, your file is too large.";
-				} else {
-				
-					if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif"){
-						echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-					} else {
-			
-						if(move_uploaded_file($profile_picture['tmp_name'], $target_path)){
-							$data = array($name, $email, $unique_file_name);
-							$file = fopen("users.csv", "a");
-							fputcsv($file, $data);
-							fclose($file);
-							setcookie("username", $name, time() + (86400 * 30), "/");
-							header("Location: users.php");
-						} else {
-							echo "Sorry, there was an error uploading your file.";
-						}
-					}
-				}
-			}
-		}
-	}
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $profilePic = $_FILES['profile-pic'];
+
+   
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format";
+        exit;
+    }
+
+    
+    $uploadsDir = 'uploads/';
+    $profilePicName = uniqid() . '_' . $profilePic['name'];
+    $targetFile = $uploadsDir . $profilePicName;
+    if (move_uploaded_file($profilePic['tmp_name'], $targetFile)) {
+        echo "Profile picture uploaded successfully";
+    } else {
+        echo "Failed to upload profile picture";
+        exit;
+    }
+
+    $timestamp = date('YmdHis');
+    $profilePicName = $timestamp . '_' . $profilePicName;
+    rename($targetFile, $uploadsDir . $profilePicName);
+
+   
+    $userData = array($name, $email, $profilePicName);
+    $fp = fopen('users.csv', 'a');
+    fputcsv($fp, $userData);
+    fclose($fp);
+
+  
+    setcookie('username', $name, time() + 3600);
+
+  
+    header('Location: users.php');
+} else {
+    echo "All fields are required";
+    exit;
 }
-
-
-$file = fopen("users.csv", "r");
-echo "<table border='1'>";
-echo "<tr><th>Name</th><th>Email</th><th
 
 
 
